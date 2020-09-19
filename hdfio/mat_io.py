e98c6f0f6a913cc084bb556641e80d615a9d1c75
RealPolitiX <xrpatrick@gmail.com>
2020-09-19 09:27:54 -0500

    Added mat_io module

---
+++ hdfio/mat_io.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from h5py import File
+from scipy.io import loadmat, savemat
+from silx.io.dictdump import dicttoh5, h5todict
+
+
+# Conversion functions

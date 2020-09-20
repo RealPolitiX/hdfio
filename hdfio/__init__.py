73e2324156a02d5a72d7e18ef3a38716c4c18f79
RealPolitiX <xrpatrick@gmail.com>
2020-09-20 12:23:28 -0500

    Updated mat_io module

--- hdfio/__init__.py
+++ hdfio/__init__.py
@@ -5,4 +5,4 @@ __version_info__ = ('0', '2', '5')
 __version__ = '.'.join(__version_info__)
 __author__ = 'R. Patrick Xian, others'
 
-from . import dict_io, json_io
+from . import dict_io, json_io, mat_io
--- hdfio/mat_io.py
+++ hdfio/mat_io.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from . import dict_io as io
 import numpy as np
 from h5py import File
 from scipy.io import loadmat, savemat
@@ -8,3 +9,22 @@ from silx.io.dictdump import dicttoh5, h5todict
 
 
 # Conversion functions
+
+def mat_to_h5(load_addr, h5_dir, **kwargs):
+    """ Convert mat file to HDF5 via dictionary.
+    """
+
+    matdict = loadmat(load_addr)
+
+    fconv = kwargs.pop('fconv', io.dict_to_h5)
+    fconv(matdict, h5_dir, **kwargs)
+
+
+def h5_to_mat(load_addr, mat_dir, **kwargs):
+    """ Convert HDF5 to mat file via dictionary.
+    """
+
+    fconv = kwargs.pop('fconv', io.h5_to_dict)
+    dct = fconv(load_addr)
+
+    savemat(dct, mat_dir, **kwargs)

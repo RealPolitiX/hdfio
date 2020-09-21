892f252d8a5449a3f0e2c636373c5ea1f35923fd
RealPolitiX <xrpatrick@gmail.com>
2020-09-21 06:28:24 -0500

    Added option to remove metadata when loading mat file

--- hdfio/mat_io.py
+++ hdfio/mat_io.py
@@ -5,26 +5,29 @@ from . import dict_io as io
 import numpy as np
 from h5py import File
 from scipy.io import loadmat, savemat
-from silx.io.dictdump import dicttoh5, h5todict
 
 
 # Conversion functions
 
-def mat_to_h5(load_addr, h5_dir, **kwargs):
+def mat_to_h5(load_addr, h5_dir, keep_meta=False, loadkwargs={}, **kwargs):
     """ Convert mat file to HDF5 via dictionary.
     """
 
-    matdict = loadmat(load_addr)
+    sqm = loadkwargs.pop('squeeze_me', True)
+    matdict = loadmat(load_addr, squeeze_me=sqm, **loadkwargs)
+    
+    if not keep_meta:
+        matdict = dict((mk, mv) for mk, mv in matdict.items() if not mk.startswith('__'))
 
     fconv = kwargs.pop('fconv', io.dict_to_h5)
     fconv(matdict, h5_dir, **kwargs)
 
 
-def h5_to_mat(load_addr, mat_dir, **kwargs):
+def h5_to_mat(load_addr, mat_dir, loadkwargs={}, **kwargs):
     """ Convert HDF5 to mat file via dictionary.
     """
 
     fconv = kwargs.pop('fconv', io.h5_to_dict)
-    dct = fconv(load_addr)
+    dct = fconv(load_addr, **loadkwargs)
 
-    savemat(dct, mat_dir, **kwargs)
+    savemat(file_name=mat_dir, mdict=dct, **kwargs)
